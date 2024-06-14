a6c82299a103a86dd840204d3d7ad96fbef512c3
Chisom <ozimmanuel79@gmail.com>
2024-06-14 09:32:33 +0100

    Add files via upload

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -5,7 +5,7 @@ document.getElementById('toggleSwitch').addEventListener('change', function () {
     console.log('Switch is ON')
     document.body.style.backgroundColor = '#359567'
     document.body.style.color = '#FFFFFF'
-    var infoLinks = document.querySelectorAll('.info a')
+    var infoLinks = document.querySelectorAll('.infolink')
     infoLinks.forEach(function (link) {
       link.style.color = 'white' // Change link color as needed
     })
@@ -17,7 +17,7 @@ document.getElementById('toggleSwitch').addEventListener('change', function () {
     console.log('Switch is OFF')
     document.body.style.backgroundColor = '#EBF2F2'
     document.body.style.color = 'darkslategrey'
-    var infoLinks = document.querySelectorAll('.info a')
+    var infoLinks = document.querySelectorAll('.infolink')
     infoLinks.forEach(function (link) {
       link.style.color = 'darkslategrey' // Change link color as needed
     })
