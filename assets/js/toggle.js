e353962b6f21c4e48589c752de3a8e1f341f9fb4
Chisom <ozimmanuel79@gmail.com>
2024-06-14 09:07:03 +0100

    Add files via upload

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -4,18 +4,26 @@ document.getElementById('toggleSwitch').addEventListener('change', function () {
   if (this.checked) {
     console.log('Switch is ON')
     document.body.style.backgroundColor = '#359567'
-      document.body.style.color = '#FFFFFF'
-      var infoLinks = document.querySelectorAll('a');
-        infoLinks.forEach(function(link) {
-            link.style.color = 'white'; // Change link color as needed
-        });
+    document.body.style.color = '#FFFFFF'
+    var infoLinks = document.querySelectorAll('.info a')
+    infoLinks.forEach(function (link) {
+      link.style.color = 'white' // Change link color as needed
+    })
+    var infoLinks = document.querySelectorAll('.menuinfolink')
+    infoLinks.forEach(function (link) {
+      link.style.color = 'white' // Change link color as needed
+    })
   } else {
     console.log('Switch is OFF')
     document.body.style.backgroundColor = '#EBF2F2'
-      document.body.style.color = 'darkslategrey'
-      var infoLinks = document.querySelectorAll('a');
-        infoLinks.forEach(function(link) {
-            link.style.color = 'darkslategrey'; // Change link color as needed
-        });
+    document.body.style.color = 'darkslategrey'
+    var infoLinks = document.querySelectorAll('.info a')
+    infoLinks.forEach(function (link) {
+      link.style.color = 'darkslategrey' // Change link color as needed
+    })
+      var infoLinks = document.querySelectorAll('.menuinfolink')
+    infoLinks.forEach(function (link) {
+      link.style.color = '' // Change link color as needed
+    })
   }
 })
