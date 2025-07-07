af4feedb48af58639dbffcc75875352aba445353
Chisom Ozoemena <ozimmanuel79@gmail.com>
2025-07-07 08:35:11 +0100

    Update tab.js

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -1,14 +1,14 @@
 function toggleTab(tabId) {
-  var tabcontent = document.getElementsByClassName("tabcontent");
-  for (var i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "hidden";
-  }
+      var tabcontent = document.getElementsByClassName("tabcontent");
+      for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
 
-  var selectedTab = document.getElementById(tabId);
-  selectedTab.style.display = "block";
-  
+      var selectedTab = document.getElementById(tabId);
+      selectedTab.style.display = "block";
+      
   var nav = document.getElementsByClassName("nav");
   if (tabId === 'tab2') {
-      nav.style.display = "hidden";
+      nav.style.display = "none";
   }
 }
